e9ed61de8fb5a14700195eaf1363bebd044a4d4c
Nullpointer <nullpointerworks@outlook.com>
2020-04-20 08:26:48 +0200

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,7 @@
  * Creative Commons - Attribution, Share Alike 4.0<br>
  * Nullpointer Works (2019)<br>
  * Use of this library is subject to license terms.<br>
- * @version 0.7.2 experimental
+ * @version 0.8.0 experimental
  * @author Michiel Drost - Nullpointer Works
  */
 module libnpw.xml
