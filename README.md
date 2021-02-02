08a24e5be09cd6bd74b8da9c6e8c923e7b8fecee
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-02-02 10:42:00 +0100

    version

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ An XML/HTML library to read and write XML formatted text files.
 * exports exp.nullpointerworks.xml
 * exports exp.nullpointerworks.xml.format
 * exports exp.nullpointerworks.xml.io
+* exports exp.nullpointerworks.xml.io.dom
+* exports exp.nullpointerworks.xml.io.sax
 * exports exp.nullpointerworks.xml.prolog
 
-
-# xml v0.14.0 experimental
+# xml v0.15.0 experimental
 * util 1.0.x
--- src/module-info.java
+++ src/module-info.java
@@ -2,14 +2,17 @@
  * Creative Commons - Attribution, Share Alike 4.0<br>
  * Nullpointer Works (2021)<br>
  * Using this library makes you subject to the license terms.<br>
- * @version 0.14.0 experimental
+ * @version 0.15.0 experimental
  * @author Michiel Drost - Nullpointer Works
  */
 module libnpw.xml
 {
 	requires transitive libnpw.util;
+	
 	exports exp.nullpointerworks.xml;
 	exports exp.nullpointerworks.xml.format;
 	exports exp.nullpointerworks.xml.io;
+	exports exp.nullpointerworks.xml.io.dom;
+	exports exp.nullpointerworks.xml.io.sax;
 	exports exp.nullpointerworks.xml.prolog;
 }
